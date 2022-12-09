f04142a9ca5c4860672e9c16255413e768782226
woosun park <zoe0526@naver.com>
2022-12-10 03:35:53 +0900

    monsterconroller 수정

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -26,42 +26,61 @@ public class MonsterStat : MonoBehaviour
 public class MonsterController : MonsterStat
 {
     int move_dir = 1;
+    float pase_time=0f;
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            transform.GetComponent<Animator>().Play("Attack", -1, 0f);
+            if(transform.position.x>collision.gameObject.transform.position.x)
+                transform.localScale = new Vector3(-1, 1, 1);
+            else
+                transform.localScale = new Vector3(1, 1, 1);
+            StartCoroutine(monster_attack_sequence());
         }
 
     }
+    IEnumerator monster_attack_sequence()
+    {
+        transform.GetComponent<Animator>().Play("Attack", -1, 0f);
+        StopCoroutine(move_coroutine);
+        move_coroutine = null;
+        yield return new WaitForSeconds(.1f);
+        transform.localScale = new Vector3(move_dir, 1, 1);
+        move_coroutine = move_routine();
+        StartCoroutine(move_coroutine);
 
+    }
+
+    IEnumerator move_coroutine;
     // Start is called before the first frame update
     void Start()
     {
-        transform.GetComponent<Animator>().Play("Move", -1, 0f);
         move_dir = 1;
         //set_monster_stat();
         transform.localScale = Vector3.one;
-        StartCoroutine(move_routine());
+        move_coroutine = move_routine();
+        StartCoroutine(move_coroutine);
+        pase_time = _pase_time;
     }
 
     IEnumerator move_routine()
     {
+        transform.GetComponent<Animator>().Play("Move", -1, 0f);
         while (true)
         {
             transform.localPosition += new Vector3(move_dir*_move_speed, 0, 0);
             yield return new WaitForSeconds(.1f);
-            _pase_time -= .1f;
-            if(_pase_time <= 0)
+            pase_time -= .1f;
+            if(pase_time <= 0)
             {
                 if(move_dir==1)
                     move_dir = -1;
                 else
                     move_dir = 1;
                 transform.localScale = new Vector3(move_dir, 1, 1);
-                _pase_time = 2f;
+                pase_time = _pase_time;
             }
 
         }
